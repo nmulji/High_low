b4ce77ed605922f8630d78a8a8371e5611ae30fa
Niraj <niraj.mulji@gmail.com>
2016-04-15 14:50:41 -0500

    This is my completed high low game.

---
+++ high_low.php
@@ -0,0 +1,22 @@
+<?php
+
+$count = 0;
+$min = 1;
+$max = 100;
+$random_number = rand($min , $max);
+
+echo "Guess a number between 1-100\n";
+
+do {
+	fwrite(STDOUT, "What's your guess?\n");
+	$user_guess = trim(fgets(STDIN));
+	$count++;
+	if ($user_guess == $random_number) {
+		echo "YOU WON!\n";
+		echo "It took you ". $count++ . " guesses!\n";
+	} elseif ($user_guess > $random_number) {
+		echo "LOWER!\n";
+	} else {
+		echo "HIGHER\n";
+	}
+} while ($user_guess != $random_number);
