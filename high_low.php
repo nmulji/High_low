9073c9192bb8c0ce9c6486f4e8f87260d2b643dc
Niraj <niraj.mulji@gmail.com>
2016-04-15 15:46:09 -0500

    This is my high low game after I added another do while loop, so the user can play the game over and over again.

--- high_low.php
+++ high_low.php
@@ -1,22 +1,29 @@
 <?php
 
-$count = 0;
 $min = 1;
 $max = 100;
-$random_number = mt_rand($min , $max);
 
-echo "Guess a number between 1-100\n";
 
 do {
-	fwrite(STDOUT, "What's your guess?\n");
-	$user_guess = trim(fgets(STDIN));
-	$count++;
-	if ($user_guess == $random_number) {
-		echo "YOU WON!\n";
-		echo "It took you ". $count++ . " guesses!\n";
-	} elseif ($user_guess > $random_number) {
-		echo "LOWER!\n";
-	} else {
-		echo "HIGHER\n";
-	}
-} while ($user_guess != $random_number);
+	$count = 0;
+	$random_number = mt_rand($min , $max);
+	echo "Guess a number between 1-100\n";
+
+	do {
+		fwrite(STDOUT, "What's your guess?\n");
+		$user_guess = trim(fgets(STDIN));
+		$count++;
+		if ($user_guess == $random_number) {
+			echo "YOU WON!\n";
+			echo "It took you ". $count++ . " guesses!\n";
+		} elseif ($user_guess > $random_number) {
+			echo "LOWER!\n";
+		} else {
+			echo "HIGHER\n";
+		}
+	} while ($user_guess != $random_number);
+
+	fwrite(STDOUT, "Play Again?\n");
+	$play_again = trim(fgets(STDIN));
+
+} while ($play_again == "yes");
